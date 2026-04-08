ab6589efcadf6bb3ce6810b09fa68e1c66540203
Federico Viera <fedviera@gmail.com>
2026-04-08 00:40:38 -0300

    Hotfix: S3

--- project/helpers/s3.py
+++ project/helpers/s3.py
@@ -25,9 +25,6 @@ class S3Service:
                 kwargs["aws_secret_access_key"] = os.getenv(
                     "AWS_SECRET_ACCESS_KEY", "test"
                 )  # noqa: S105
-            else:
-                kwargs["aws_access_key_id"] = os.getenv("AWS_ACCESS_KEY_ID")
-                kwargs["aws_secret_access_key"] = os.getenv("AWS_SECRET_ACCESS_KEY")
             cls._client = boto3.client("s3", **kwargs)
         return cls._client
 
