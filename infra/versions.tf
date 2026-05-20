9705403e61064c72a2eaba6691781d13f5eb37ba
naprado <naprado@itba.edu.ar>
2026-05-20 15:36:27 -0300

    Feat: Update Terraform workflows to support remote backend configuration and improve initialization process

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,12 +1,6 @@
 terraform {
   required_version = ">= 1.6.0"
 
-  backend "s3" {
-    key     = "abricot-be/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-
   required_providers {
     archive = {
       source  = "hashicorp/archive"
